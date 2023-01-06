63db1a094e2288d24cc621217d5de3c27523d796
pk050101 <pk.it050101@gmail.com>
2023-01-07 01:28:28 +0530

    "predeploy": "npm run build", +   "deploy": "gh-pages -d build",

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio Websitepm s</h1>
+      <h1>Portfolio Website Running</h1>
     </div>
   );
 }
